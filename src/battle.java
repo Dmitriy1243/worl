a104532bd0fe7eed0fe2afc037912048e44ea1cc
Dmitriy1243 <121992771+Dmitriy1243@users.noreply.github.com>
2023-04-12 00:28:07 +0300

    Initial commit

--- src/battle.java
+++ src/battle.java
@@ -1,4 +1,5 @@
 public class battle {
+<<<<<<< HEAD
     static int zhp;
     static int zf;
     static int zd;
@@ -7,11 +8,15 @@ public class battle {
     static int pf;
     static int pd;
     static boolean pl;
+=======
+
+>>>>>>> ff96108 (Initial commit)
     static int damagez;
     static int damagep;
 
 
     public void battle(Person person, Zombie zombie) {
+<<<<<<< HEAD
         zhp = zombie.hp;
         zf = zombie.force;
         zd = zombie.dexterity;
@@ -28,15 +33,29 @@ public class battle {
 
 
         for( ; zhp >= 0 && php >= 0 ; zhp = zhp - damagep, php = php -damagez )
+=======
+
+        damagez = zombie.force * zombie.dexterity;
+        damagep = person.force * person.dexterity;
+
+        for( ; zombie.hp >= 0 && person.hp >= 0 ; zombie.hp = zombie.hp - damagep, person.hp = person.hp -damagez )
+>>>>>>> ff96108 (Initial commit)
 
     {
         System.out.println("bym");
         System.out.println("bam");
     }
+<<<<<<< HEAD
         if ( zhp <= 0 ){
             System.out.println(" Person Win ");
             person.xp = person.xp + 15;
             person.hp = php;
+=======
+        if ( zombie.hp <= 0 ){
+            System.out.println(" Person Win ");
+            person.xp = person.xp + 15;
+
+>>>>>>> ff96108 (Initial commit)
             person.coint = person.coint + 5;
         } else  {
             System.out.println( " Zombie win ");
--- src/battle_3.java
+++ src/battle_3.java
@@ -1,4 +1,5 @@
 public class battle_3 {
+<<<<<<< HEAD
     static int rhp;
     static int rf;
     static int rd;
@@ -7,10 +8,14 @@ public class battle_3 {
     static int pf;
     static int pd;
     static boolean pl;
+=======
+
+>>>>>>> ff96108 (Initial commit)
     static int damager;
     static int damagep;
 
     public void battle(Person person, Rooster rooster) {
+<<<<<<< HEAD
         rhp = rooster.hp;
         rf = rooster.force;
         rd = rooster.dexterity;
@@ -23,15 +28,29 @@ public class battle_3 {
         damagep = pf * pd;
 
         for(; rhp >= 0 && php >= 0; rhp = rhp - damagep, php = php - damager)
+=======
+
+        damager = rooster.force * rooster.dexterity;
+        damagep = person.force * person.dexterity;
+
+        for(; rooster.hp >= 0 && person.hp >= 0; rooster.hp = rooster.hp - damagep, person.hp = person.hp - damager)
+>>>>>>> ff96108 (Initial commit)
 
         {
             System.out.println("bym");
             System.out.println("bam");
         }
+<<<<<<< HEAD
         if ( rhp <= 0 ){
             System.out.println(" Person Win ");
             person.xp = person.xp + 10;
             person.hp = php;
+=======
+        if ( rooster.hp <= 0 ){
+            System.out.println(" Person Win ");
+            person.xp = person.xp + 10;
+
+>>>>>>> ff96108 (Initial commit)
             person.coint = person.coint + 10;
         } else  {
             System.out.println( " rooster win ");
