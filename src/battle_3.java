e557d5069f55012ebe875d4994b4954979e7d6b3
Dmitriy1243 <121992771+Dmitriy1243@users.noreply.github.com>
2023-04-13 23:06:33 +0300

    Initial commit

--- src/battle_3.java
+++ src/battle_3.java
@@ -1,42 +1,27 @@
 public class battle_3 {
-
-    static int rhp;
-    static int rf;
-
-    static boolean rl;
-    static int php;
-    static int pf;
-    static int pd;
     static boolean pl;
-
     static int damager;
     static int damagep;
 
     public void battle(Person person, Rooster rooster) {
-        rhp = rooster.hp;
-        rf = rooster.force;
 
-        rl = rooster.life;
-        php = person.hp;
-        pf = person.force;
-        pd = person.dexterity;
         pl = person.life;
         damager = rooster.force * rooster.dexterity;
-        damagep = pf * pd;
+        damagep = person.force * person.dexterity;
 
-        for (; rhp >= 0 && php >= 0; rhp = rhp - damagep, php = php - damager)
+        for (; rooster.hp >= 0 && person.hp >= 0; rooster.hp = rooster.hp - damagep, person.hp = person.hp - damager)
 
             damager = rooster.force * rooster.dexterity;
-        damagep = person.force * person.dexterity;
+            damagep = person.force * person.dexterity;
 
         for (; rooster.hp >= 0 && person.hp >= 0; rooster.hp = rooster.hp - damagep, person.hp = person.hp - damager) {
             System.out.println("bym");
             System.out.println("bam");
         }
-        if (rhp <= 0) {
+        if (rooster.hp <= 0) {
             System.out.println(" Person Win ");
             person.xp = person.xp + 10;
-            person.hp = php;
+
             if (rooster.hp <= 0) {
                 System.out.println(" Person Win ");
                 person.xp = person.xp + 10;
